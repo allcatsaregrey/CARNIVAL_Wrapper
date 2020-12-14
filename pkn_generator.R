ffee789321e60dd571187f5d2c61a80489f25fb0
Matt McFee <mattcmcfee@gmail.com>
2020-12-14 00:17:58 -0500

    Additional functionality added based on docs for DoRothEA and PROGENy. Pretty plotting and processing of the TF activations for use in CARNIVAL pipeline need to be completed

--- pkn_generator.R
+++ pkn_generator.R
@@ -3,12 +3,22 @@
 # OmnipathDB web database interface and then perform CARNIVAL analysis with said 
 # PKN. This is mostly adapted form the examples provided by the Saez lab group.
 
-# TODO: Streamline adding network infromation for nodes that do not exist in
-# the OmnipathDB network.
+# TODO: Streamline adding network information for nodes that do not exist in
+# the OmnipathDB network. Format DoRothEA and PROGENy data so they run with the
+# CARNIVAL pipeline.
 
 # Library imports
 req_pac <- c("dplyr", "ggplot2", "OmnipathR", "igraph", "ggraph", "GGally", 
-             "CARNIVAL", "Rgraphviz")
+             "CARNIVAL", "Rgraphviz", "dorothea", "progeny", "viper", "Seurat",
+             "tcltk", "biomaRt")
+
+# Force package install
+for (pac in req_pac) {
+  if(!pac %in% installed.packages()) {
+    install.packages(i, dependencies = TRUE)
+  }
+}
+
 lapply(req_pac, require, character.only = TRUE)
 
 # click on the 'interactions' variable to view the proteins included
@@ -23,6 +33,64 @@ user_net <-  interaction_graph(interactions = interactions)
 net_head <- function(interactions, n) {
   
 print_interactions(head(interactions, n))
+  
+}
+
+proc_data <- function(find_weights = FALSE) {
+  
+  # See dbellViper via data(bcellViper, package = "bcellViper") for sample 
+  # data format for dor_mat
+  dor_dat <- read_csv(tk_choose.files(caption = "Import data for DoRothEA"))
+  #
+  prog_dat <- read_csv(tk.choose.files(caption = "Import data for PROGENy"))
+  
+  
+  # Select regulons with high confidence values of "A" and "B"
+  regulons <- data(dorothea_hs, package = "dorothea") %>%
+    filter(confidence %in% c("A", "B"))
+  
+  # Bulk RNAseq data can be processed this way
+  tf_activities <- run_viper(dor_dat, regulons, 
+                             options =  list(method = "scale", minsize = 4, 
+                                             eset.filter = FALSE, cores = 1, 
+                                             verbose = FALSE))
+  
+  # Bulk RNAseq data processing for PROGENy
+  # Import data to DESeq2 and variance stabilize
+  
+  prog_pathways <- list()
+  
+  if(find_weights == TRUE) {
+    dset <- DESeqDataSetFromMatrix(prog_dat,
+                                colData = as.data.frame(colData(prog_dat)), 
+                                design =~ dex)
+    dset <- estimateSizeFactors(dset)
+    dset <- estimateDispersions(dset)
+    gene_expr <- getVarianceStabilizedData(dset)
+    
+    # Annotate matrix with HGNC symbols
+    mart = useDataset("hsapiens_gene_ensembl", useMart("ensembl"))
+    genes = getBM(attributes = c("ensembl_gene_id","hgnc_symbol"),
+                values=rownames(gene_expr), mart = mart)
+    matched = match(rownames(gene_expr), genes$ensembl_gene_id)
+    rownames(gene_expr) = genes$hgnc_symbol[matched]
+    
+    # Obtain pathway scores
+    pathways <- progeny(gene_expr, scale=FALSE)
+    # Need to adjust name of control column as necessary
+    controls <- prog_dat$dex == "untrt"
+    ctl_mean <- apply(pathways[controls,], 2, mean)
+    ctl_sd <- apply(pathways[controls,], 2, sd)
+    pathways <- t(apply(pathways, 1, function(x) x - ctl_mean))
+    prog_pathways <- apply(pathways, 1, function(x) x / ctl_sd)
+    
+    return(tf_activities, prog_pathways)
+    
+  }
+  
+  # Process the data for use in CARNIVAL
+  # INCOMPLETE
+  return(tf_activities)
   
 }
 
@@ -80,8 +148,6 @@ gen_pkn <- function(user_net, sources, targets) {
   
 }
 
-
-
 gen_undirected <- function(user_graph, target) {
   
 user_graph_undirected <- as.undirected(user_graph, mode = c("mutual"))
@@ -113,25 +179,16 @@ process_net <- function(user_graph) {
     rename(from = Source, consensus_direction = Effect, to = Target)
 }
 
-# Read in appropriate data 
-read_user_data <- function() {
-  
-  user_measure <- read_csv(file.choose())
-  user_input <- read_csv(file.choose())
-  
-  return(c(user_measure, user_input))
-  
-}
 
 # Process the network, and user measurements using carnival
-carn_pkn <- function(user_net, user_measure, user_input) {
+carn_pkn <- function(user_net, user_measure, user_weight = NULL) {
   
-  result = runCARNIVAL(inputObj = user_input, measObj = user_measure,
-                       netObj = user_net)
+  result = runCARNIVAL(measObj = user_measure, netObj = user_net,
+                       weightObj = user_weight, dir_name = getwd())
   
 }
 
-# Visualize the output of the CARNIVAL pipeline with RGraphviz
+# Custom visualization for CARNIVAL results
 vis_carn <- function(result) {
   
   # Prepare the result data for plotting
--- run_pipeline.R
+++ run_pipeline.R
@@ -4,12 +4,13 @@ source("pkn_generator.R")
 runPipeline <- function(net_sources, sources, targets) {
   
   # Input the data and generate the necessary PKN
-  user_data <- read_user_data()
+  user_data <- proc_data()
   user_net <- init_net(net_sources) %>% process_net()
   user_pkn <- gen_pkn(user_net, sources, targets)
   
   # Run CARNIVAL and display the resultant network
   result <- carn_pkn(user_net, user_data[1], user_data[2])
   vis_carn(result) 
+  return(result)
   
 }
